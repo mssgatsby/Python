00449e7fe921e700d2ff0d18c1dfac4e6c70d430
mssgatsby <mssgatsby@gmail.com>
2024-06-24 14:41:10 +0500

    New string methods

--- string_methods.py
+++ string_methods.py
@@ -21,14 +21,35 @@ print(cenStr)  # _______Hello________
 #count
 str4 = "I love apples, apple are my favorite fruit"
 couStr = str4.count("apple", 10, 24)
-print(couStr) # 1
+print(couStr)  # 1
 
 #endswith
 str5 = "Hello, welcome to my world."
 endStr = str5.endswith("my world.", 5, 11)
 print(endStr)  # True, False
 
-#find
-txt = "Hello, welcome to my world."
-x = txt.find("e", 5, 10)
-print(x)
+#find vs index
+str6 = "Hello, welcome to my world."
+findStr = str6.find("e", 5, 10)
+indStr = str6.index("e", 2, 17)
+print(findStr)  # return -1 if there is not
+print(indStr)  # return ValueError if there is not
+
+#format
+txt1 = "My name is {fname}, I'm {age}".format(fname="John", age=36)
+txt2 = "My name is {0}, I'm {1}".format("John", 36)
+txt3 = "My name is {}, I'm {}".format("John", 36)
+
+#isidentifier
+str7 = "Demo1_2"
+isidStr = str7.isidentifier()
+print(isidStr)
+
+# isdigit(): Recognizes only decimal digits (0-9).
+# isnumeric(): Recognizes a broader range of numeric characters including decimal digits, Unicode numeric characters, fractions, subscripts, superscripts, etc.
+
+# there is join method, returns added tuples
+str9 = ("John", "Peter", "Vicky")
+joinstr = "#".join(str9)
+print(joinstr)
+# if dict, they join only keys, not values
