60ec0f983073c502dcfb1fda67e49a3317d0d6de
mssgatsby <mssgatsby@gmail.com>
2024-06-24 13:52:18 +0500

    Resolve merge conflicts

--- string_methods.py
+++ string_methods.py
@@ -12,4 +12,23 @@ print(casStr)  # hello world
 print(lowStr)  # hello world
 # lower only converts ASCII based characters. casefold converts other (German based or other language unique characters)
 # types to lowercase as well. Lower works faster
-# gello worldhsbhsdhb
+
+#center
+str3 = "Hello"
+cenStr = str3.center(20, "_")
+print(cenStr)  # _______Hello________
+
+#count
+str4 = "I love apples, apple are my favorite fruit"
+couStr = str4.count("apple", 10, 24)
+print(couStr) # 1
+
+#endswith
+str5 = "Hello, welcome to my world."
+endStr = str5.endswith("my world.", 5, 11)
+print(endStr)  # True, False
+
+#find
+txt = "Hello, welcome to my world."
+x = txt.find("e", 5, 10)
+print(x)
