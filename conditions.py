fc5c0788b4fff7b31a23435084db36056e30c752
mssgatsby <mssgatsby@gmail.com>
2024-07-09 09:56:22 +0500

    conditions learned

---
+++ conditions.py
@@ -0,0 +1,38 @@
+# num = int(input('son: '))
+# if num < 0:
+#     print('manfiy son')
+# else:
+#     print('musbat son')
+#
+# if num % 2 == 0:
+#     print('juft son')
+# else:
+#     print('toq son')
+#
+# if num < 0:
+#     print(num - 2)
+# else:
+#     print(num + 2)
+
+# num1 = float(input('son1: '))
+# num2= float(input('son: '))
+
+# if num1 > num2:
+#     print(num1, num2)
+# else:
+#     print(num2, num1)
+
+# if num1 % 1< num2 % 1:
+#     print(num1)
+# else:
+#     print(num2)
+
+num1 = int(input('son1: '))
+num2 = int(input('son2: '))
+num3 = int(input('son3: '))
+
+# if num1 > num2 > num3 or num1 < num2 < num3:
+#     print(True)
+# else:
+#     print(False)
+
